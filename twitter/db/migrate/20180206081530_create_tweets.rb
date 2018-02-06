466add0ae332aa4bf5e12e0f456ea926608c82d6
priyankanaren96@gmail.com <priyankanaren96@gmail.com>
2018-02-06 13:48:34 +0530

    scaffold_tweet

---
+++ twitter/db/migrate/20180206081530_create_tweets.rb
@@ -0,0 +1,12 @@
+class CreateTweets < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tweets do |t|
+      t.string :username
+      t.string :text
+      t.string :status
+      t.string :approvedby
+
+      t.timestamps
+    end
+  end
+end
