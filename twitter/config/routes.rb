5391d3e1fbc82ab816b47c7a30590f9a76b2f02f
priyankanaren96@gmail.com <priyankanaren96@gmail.com>
2018-02-07 10:20:55 +0530

    tweet_user_update

--- twitter/config/routes.rb
+++ twitter/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :tweets
+get "/indexn" => "tweets#indexn"  
+resources :tweets
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
