55208817226899811d2bf7ffcba1e3b88468776c
priyankanaren96@gmail.com <priyankanaren96@gmail.com>
2018-02-08 13:11:50 +0530

    priyanka update

--- twitter/config/routes.rb
+++ twitter/config/routes.rb
@@ -8,13 +8,13 @@ resources :tweets do
 end
 
   resources :users
-=======
+
 
   #resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    
-
+  get '/admin' => "users#admin"
   post 'users/login'
   get "/check_user" => "users#check_user"
   get "/checkmail_user" => "users#checkmail_user"
